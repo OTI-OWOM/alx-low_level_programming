192f8e85a36813e67036f866eaf390762f6dffcb
OTI-OWOM <ezeogouba@gmail.com>
2024-06-20 05:33:56 -0700

    I added Puts(Task4)

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description: Calls puts and receives value returned by puts after
+ *its execution.
+ *Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+  puts ("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
