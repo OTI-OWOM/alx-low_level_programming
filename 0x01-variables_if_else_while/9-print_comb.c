271a6a4d1940eb38b7cdd568087b0db1f280987a
OTI-OWOM <ezeogouba@gmail.com>
2024-06-22 06:18:55 -0700

    Write a program that prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Write a program that prints all possible combinations of 
+ * main - Write a program that prints all possible combinations of
  *single-digit numbers.
  *
  *Return: Always 0 (Sucess)
