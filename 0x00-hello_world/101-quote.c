f4c1998538ce7f0b0b532ed6d5a5df098621ba7e
OTI-OWOM <ezeogouba@gmail.com>
2024-06-24 05:43:51 -0700

    added task 8 unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main -Entry point
- *Return: Always 1 (Sucess)
+ *Return: 1 if Sucess
  */
 int main(void)
 {
-write(2,"and that piece of art is useful\"-Dora Korper,2015-10-19\n", 59);
+write(2, "and that piece of art is useful - Dora Korper,2015-10-19\n", 59);
 return (1);
 }
