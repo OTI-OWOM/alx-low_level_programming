8b8a93dbd965f276e6f7f816f8134429856c78fd
OTI-OWOM <ezeogouba@gmail.com>
2024-06-25 14:11:40 -0700

    a function that adds two integers and returns the result.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+/**
+ * add - Adds two values
+ *@n1: The first number to be computed
+ *@n2: The second number to be computed
+ *
+ * Return: void
+ */
+
+int add(int n1, int n2)
+{
+
+return (n1 + n2);
+
+}
