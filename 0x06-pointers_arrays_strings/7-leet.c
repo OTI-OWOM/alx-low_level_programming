dc8cbbf75400ac4a0e78f08d823d313b3e33b239
OTI-OWOM <ezeogouba@gmail.com>
2024-07-06 02:25:12 -0700

    added 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * leet - encodes a string into 1337.
+ *
+ * @s: string to code
+ * Return: the encoded string
+ */
+
+char *leet(char *s)
+{
+int a;
+int b;
+int c;
+
+char l[] = "oOlLeEaAtT";
+char e[] = "0011334477";
+
+a = 0;
+while (s[a] != '\0')
+{
+b = 0;
+c = 0;
+while (l[b] != '\0')
+{
+if (s[a] == l[b])
+{
+c = b;
+s[a] = e[c];
+}
+b++;
+}
+a++;
+}
+return (s);
+}
