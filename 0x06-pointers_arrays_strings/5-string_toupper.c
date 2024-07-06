09b73a33ff91d2c4f9b70edc9a1ccaeeeb1e1472
OTI-OWOM <ezeogouba@gmail.com>
2024-07-06 00:50:46 -0700

    added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * string_toupper - lowercase letters of string to upper
+ * @c: this the input string 
+ *
+ *
+ * Return: lowercase changed to uppercase of the string
+ */
+
+char *string_toupper(char *c)
+
+{
+int index;
+int letter = 'a' - 'A';
+
+for (index = 0; c[index] != '\0'; ++index)
+{
+if (c[index] >= 'a' && c[index] <= 'z')
+{
+c[index] = c[index] - letter;
+}
+}
+return c;
+
+}
