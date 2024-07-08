fc152c69861aebb441304a37f8826520e3dc29e5
OTI-OWOM <ezeogouba@gmail.com>
2024-07-08 06:50:19 -0700

    added 100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,22 +5,18 @@
  * @s: An input string to encode using rot13
  * Return: An encode string
  */
-
 char *rot13(char *s)
 {
 int i = 0;
 
 while (s[i] != '\0')
 {
-
 while ((s[i] >= 'a' && s[i] <= 'z') ||
 (s[i] >= 'A' && s[i] <= 'Z'))
 {
-
 if ((s[i] >= 'a' && s[i] <= 'm') ||
 (s[i] >= 'A' && s[i] <= 'M'))
 s[i] += 13;
-
 else
 s[i] -= 13;
 i++;
@@ -29,3 +25,4 @@ i++;
 }
 return (s);
 }
+
