e13a5b6a0e39f1d21aae680b470109aa40db49dc
OTI-OWOM <ezeogouba@gmail.com>
2024-07-15 18:15:11 -0700

    Created the static library libmy.a

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+char *_memeset(char *s, char b, unsigned int n)
+{
+return memset(s, b, n);
+}
