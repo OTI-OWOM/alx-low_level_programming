cc1232128a87d3410aa34b92ae36a6cffbddf545
OTI-OWOM <ezeogouba@gmail.com>
2024-07-26 08:57:49 -0700

    Added Task 3 -Write a function that prints anything.

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -6,15 +6,13 @@
  *@format: list of types of arguments passed to the function
  *
  */
+
 void print_all(const char * const format, ...)
 {
 int i = 0;
 char *str, *sep = "";
-
 va_list list;
-
 va_start(list, format);
-
 if (format)
 {
 while (format[i])
@@ -24,15 +22,12 @@ switch (format[i])
 case 'c':
 printf("%s%c", sep, va_arg(list, int));
 break;
-
 case 'i':
 printf("%s%d", sep, va_arg(list, int));
 break;
-
 case 'f':
 printf("%s%f", sep, va_arg(list, double));
 break;
-
 case 's':
 str = va_arg(list, char *);
 if (!str)
