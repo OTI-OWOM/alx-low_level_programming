05a69cb6e941c2afb2d69681871357ea07a71e95
OTI-OWOM <ezeogouba@gmail.com>
2024-09-22 07:49:30 -0700

    Added task 0

---
+++ 0x17-doubly_linked_lists/_putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+* _putchar - writes the charater c to stdout
+* @c: the character to print
+*
+* Return: On sucess 1.
+* On error, -1 is returned and errno is set appropriately.
+*/
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+
