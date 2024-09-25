3b7b9381d07ef0de742df0a13fd21516efd4e8a9
OTI-OWOM <ezeogouba@gmail.com>
2024-09-25 06:14:24 -0700

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget -P ../ https://github.com/OTI-OWOM/alx-low_level_programming \ 
+wget -P ../ https://github.com/OTI-OWOM/alx-low_level_programming
 /raw/main/0x18-dynamic_libraries/jackpot.so
 export LD_PRELOAD=../jackpot.so
